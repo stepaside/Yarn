d5e17021489ae958ea07cd3e5c091ca6ea5fa865
Max <mstepanskiy@yahoo.com>
2013-11-04 11:41:43 -0500

    Cleaned up NHibernate provider

--- Yarn.NHibernate/Data/NHibernateProvider/SqlClient/SqlDataContext.cs
+++ Yarn.NHibernate/Data/NHibernateProvider/SqlClient/SqlDataContext.cs
@@ -1,14 +1,15 @@
-﻿using Yarn.Data.NHibernateProvider.SqlClient.Dialects;
-using FluentNHibernate.Cfg.Db;
+﻿using FluentNHibernate.Cfg.Db;
+using NHibernate.Dialect;
 
 namespace Yarn.Data.NHibernateProvider.SqlClient
 {
-    public class SqlDataContext : NHibernateDataContext<MsSqlConfiguration, MsSqlConnectionStringBuilder, MsSql2008DialectWithFullTextSupport>
+    public class SqlDataContext<TDialect> : NHibernateDataContext<MsSqlConfiguration, MsSqlConnectionStringBuilder, TDialect>
+        where TDialect : MsSql2000Dialect
     {
-        public SqlDataContext() : this(null) { }
+        public SqlDataContext(MsSqlConfiguration configuration) : this(configuration, null) { }
 
-        public SqlDataContext(string contextKey = null)
-            : base(MsSqlConfiguration.MsSql2008, contextKey)
+        public SqlDataContext(MsSqlConfiguration configuration, string contextKey = null)
+            : base(configuration, contextKey)
         { }
 
         protected override string DefaultPrefix
@@ -18,5 +19,32 @@ namespace Yarn.Data.NHibernateProvider.SqlClient
                 return "NHibernate.SqlClient";
             }
         }
+    }
+
+    public class Sql2005DataContext : SqlDataContext<MsSql2005Dialect>
+    {
+        public Sql2005DataContext() : this(null) { }
+
+        public Sql2005DataContext(string contextKey = null)
+            : base(MsSqlConfiguration.MsSql2005, contextKey)
+        { }
+    }
+
+    public class Sql2008DataContext : SqlDataContext<MsSql2008Dialect>
+    {
+        public Sql2008DataContext() : this(null) { }
+
+        public Sql2008DataContext(string contextKey = null)
+            : base(MsSqlConfiguration.MsSql2008, contextKey)
+        { }
+    }
+
+    public class Sql2012DataContext : SqlDataContext<MsSql2012Dialect>
+    {
+        public Sql2012DataContext() : this(null) { }
+
+        public Sql2012DataContext(string contextKey = null)
+            : base(MsSqlConfiguration.MsSql2008, contextKey)
+        { }
     }
 }
