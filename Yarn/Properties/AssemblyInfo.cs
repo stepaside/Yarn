21963f26d708297aae05ceca3eaadd80f22f0b69
Max Stepanskiy <mstepanskiy@asicentral.com>
2014-02-17 17:50:47 -0500

    Updated Yarn's version and .nuspec files

--- Yarn/Properties/AssemblyInfo.cs
+++ Yarn/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Yet Another Repository for .Net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stepaside, Inc.")]
-[assembly: AssemblyProduct("Nemo")]
-[assembly: AssemblyCopyright("Copyright © Max Stepanskiy 2013")]
+[assembly: AssemblyProduct("Yarn")]
+[assembly: AssemblyCopyright("Copyright © Max Stepanskiy 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.1.0")]
-[assembly: AssemblyFileVersion("0.3.1.0")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
