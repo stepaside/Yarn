a8dfd70120f8fe602583af46705f3e4a18a319bb
Max Stepanskiy <mstepanskiy@asicentral.com>
2015-10-06 10:34:10 -0400

    Added ability to work with sequences in EF

---
+++ Yarn.EF/Data/EntityFrameworkProvider/DefaultSequenceNameProvider.cs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yarn.Data.EntityFrameworkProvider
+{
+    public class DefaultSequenceNameProvider : ISequenceNameProvider
+    {
+        public string GetSequenceName(Type entityType)
+        {
+            return entityType.Name + "Sequence";
+        }
+    }
+}
---
+++ Yarn.EF/Data/EntityFrameworkProvider/ISequenceNameProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yarn.Data.EntityFrameworkProvider
+{
+    public interface ISequenceNameProvider
+    {
+        string GetSequenceName(Type entityType);
+    }
+}
